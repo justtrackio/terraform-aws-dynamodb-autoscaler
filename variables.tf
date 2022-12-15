b76b00eb9a0ff46afa7e102a6c222f8c700e0cb5
Sebastian Struß <31846129+applike-ss@users.noreply.github.com>
2022-12-15 08:26:47 +0100

    feat: Added functionality (#1)
    ## Description
    <!--- Describe your changes in detail -->
    
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## Breaking Changes
    <!-- Does this break backwards compatibility with the current major
    version? -->
    <!-- If so, please provide an explanation why it is necessary. -->
    
    ## How Has This Been Tested?
    - [ ] I have updated at least one of the `examples/*` to demonstrate and
    validate my change(s)
    - [ ] I have tested and validated these changes using one or more of the
    provided `examples/*` projects
    <!--- Users should start with an existing example as its written, deploy
    it, then check their changes against it -->
    <!--- This will highlight breaking/disruptive changes. Once you have
    checked, deploy your changes to verify -->
    <!--- Please describe how you tested your changes -->
    - [ ] I have executed `pre-commit run -a` on my pull request
    <!--- Please see
    https://github.com/antonbabenko/pre-commit-terraform#how-to-install for
    how to install -->

---
+++ variables.tf
@@ -0,0 +1,122 @@
+variable "dynamodb_table_name" {
+  type        = string
+  description = "DynamoDB table name"
+}
+
+variable "dynamodb_indexes" {
+  type        = list(string)
+  description = "List of DynamoDB indexes"
+  default     = []
+}
+
+variable "autoscale_write_target" {
+  type        = number
+  default     = 50
+  description = "The target value for DynamoDB write autoscaling"
+}
+
+variable "autoscale_read_target" {
+  type        = number
+  default     = 50
+  description = "The target value for DynamoDB read autoscaling"
+}
+
+variable "autoscale_min_read_capacity" {
+  type        = number
+  default     = 5
+  description = "DynamoDB autoscaling min read capacity"
+}
+
+variable "autoscale_max_read_capacity" {
+  type        = number
+  default     = 20
+  description = "DynamoDB autoscaling max read capacity"
+}
+
+variable "autoscale_min_write_capacity" {
+  type        = number
+  default     = 5
+  description = "DynamoDB autoscaling min write capacity"
+}
+
+variable "autoscale_max_write_capacity" {
+  type        = number
+  default     = 20
+  description = "DynamoDB autoscaling max write capacity"
+}
+
+variable "autoscale_write_target_index" {
+  type        = number
+  default     = null
+  description = "The target value for DynamoDB write autoscaling of the index"
+}
+
+variable "autoscale_read_target_index" {
+  type        = number
+  default     = null
+  description = "The target value for DynamoDB read autoscaling of the index"
+}
+
+variable "autoscale_min_read_capacity_index" {
+  type        = number
+  default     = null
+  description = "DynamoDB autoscaling min read capacity of the index"
+}
+
+variable "autoscale_max_read_capacity_index" {
+  type        = number
+  default     = null
+  description = "DynamoDB autoscaling max read capacity of the index"
+}
+
+variable "autoscale_min_write_capacity_index" {
+  type        = number
+  default     = null
+  description = "DynamoDB autoscaling min write capacity of the index"
+}
+
+variable "autoscale_max_write_capacity_index" {
+  type        = number
+  default     = null
+  description = "DynamoDB autoscaling max write capacity of the index"
+}
+
+variable "autoscale_read_schedule" {
+  type = list(object({
+    schedule     = string
+    min_capacity = number
+    max_capacity = number
+  }))
+  description = "Provides an DynamoDB autoscaling scheduled action resource"
+  default     = []
+}
+
+variable "autoscale_read_schedule_index" {
+  type = list(object({
+    schedule     = string
+    min_capacity = number
+    max_capacity = number
+  }))
+  description = "Provides an DynamoDB autoscaling scheduled action resource"
+  default     = []
+}
+
+variable "autoscale_write_schedule" {
+  type = list(object({
+    schedule     = string
+    min_capacity = number
+    max_capacity = number
+  }))
+  description = "Provides an DynamoDB autoscaling scheduled action resource"
+  default     = []
+}
+
+variable "autoscale_write_schedule_index" {
+  type = list(object({
+    schedule     = string
+    min_capacity = number
+    max_capacity = number
+  }))
+  description = "Provides an DynamoDB autoscaling scheduled action resource"
+  default     = []
+}
