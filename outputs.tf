d22ade93a61e533c9bdd1a389b40176cf72859c3
Sebastian Struß <struss@justtrack.io>
2022-12-16 09:43:24 +0100

    fix: Outputs produced issues when no scaling was used

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "appautoscaling_read_policy_arn" {
-  value       = aws_appautoscaling_policy.read_policy[0].arn
+  value       = try(aws_appautoscaling_policy.read_policy[0].arn, "")
   description = "Appautoscaling read policy ARN"
 }
 
 output "appautoscaling_write_policy_arn" {
-  value       = aws_appautoscaling_policy.write_policy[0].arn
+  value       = try(aws_appautoscaling_policy.write_policy[0].arn, "")
   description = "Appautoscaling write policy ARN"
 }
